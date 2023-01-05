1ccff5b2cda1f40c460b9badc337bf9838e19a85
OLGA DRACH <dolchik88@mail.ru>
2023-01-05 17:29:31 +0300

    Программа упорядочивает по убыванию элементы в строках

---
+++ Ex_54/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача 54: Задайте двумерный массив. 
+//Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+void PrintArray(int[,] array)
+{
+   for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void StreamlineRows(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int count = 0; count < array.GetLength(1) -1 ; count++)
+        {
+            for (int j = 0; j < array.GetLength(1) - 1; j++)
+            {
+                if (array[i, j] < array[i, j + 1])
+                {
+                    int temp = array[i, j];
+                    array[i, j] = array[i, j + 1];
+                    array[i, j + 1] = temp;
+                }
+            }
+        }
+    }
+}
+
+int[,] CreateRandomArray(int rows, int columns, int leftRange, int rightRange)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i,j] = new Random().Next(leftRange, rightRange);
+        }
+    }
+    return array;
+}
+
+int EnterNumber(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+//запраштваем у пользователя кол-во строк и столбцов
+int rows = EnterNumber("Введите количество строк массива: ");
+int columns = EnterNumber("Введите количество столбцов массива: ");
+//создаем массив с помощью рандома
+int[,] matrix = CreateRandomArray(rows, columns, 1, 10);
+//выводим массив
+PrintArray(matrix);
+//метод упорядочивает элементы в строках
+StreamlineRows(matrix);
+//выводим измененный массив
+Console.WriteLine();
+PrintArray(matrix);
