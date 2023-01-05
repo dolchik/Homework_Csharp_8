386515c1b86b79d47ff7015ade0adab81a6d2105
OLGA DRACH <dolchik88@mail.ru>
2023-01-05 18:50:17 +0300

    Программа выводит заданный трехмерный массив и индексы его элементов

---
+++ Ex_60/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+//Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+
+void PrintArray(int[,,] array)
+{
+   for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]} ({i}, {j}, {k}) ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+int[,,] cube = {{{22, 23}, {33, 34}, {43, 45}}};
+PrintArray(cube);
